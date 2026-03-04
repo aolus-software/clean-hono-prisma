622677f4869a6a9aee66beae86898ed3ce62d7f3
zulfikar-ditya <zulfikarditya.work@gmail.com>
2026-03-04 20:29:49 +0700

    chore: lint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { AppConfig } from "@config";
 import app from "./app";
 
+// eslint-disable-next-line no-console
 console.log(`Starting server on port ${AppConfig.APP_PORT}`);
 
 export default {
